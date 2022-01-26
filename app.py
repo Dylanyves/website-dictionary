bd632bcd01350b6b78e624c60dedf1ff0c861d10
Dylan <macyvesdylan@gmail.com>
2022-01-26 10:44:22 +0700

    changes

--- app.py
+++ app.py
@@ -17,67 +17,67 @@ class Content:
             search_btn = st.form_submit_button('Search')
 
         if search_btn:
-            try:
-                st.subheader(f'Define - {word}')
-                syns = wordnet.synsets(word)
+            # try:
+            st.subheader(f'Define - {word}')
+            syns = wordnet.synsets(word)
 
-                # Check if input is valid
-                syns[0]
-                st.write('---')
+            # Check if input is valid
+            syns[0]
+            st.write('---')
 
-                for syn in syns:
-                    if syn.examples():
+            for syn in syns:
+                if syn.examples():
 
-                        # Definition
-                        definition = syn.definition()
-                        st.write(f'**{definition.capitalize().replace(";", "")}**')
+                    # Definition
+                    definition = syn.definition()
+                    st.write(f'**{definition.capitalize().replace(";", "")}**')
 
-                        # Syntax
-                        syntax = {
-                            'n':'Noun',
-                            'v':'Verb',
-                            'a':'Adjective',
-                            'r':'Adverb',
-                            's':'Singular'
-                        }
-                        try:
-                            st.write(f'_{syntax[syn.pos()]}_')
-                        except:
-                            pass
+                    # Syntax
+                    syntax = {
+                        'n':'Noun',
+                        'v':'Verb',
+                        'a':'Adjective',
+                        'r':'Adverb',
+                        's':'Singular'
+                    }
+                    try:
+                        st.write(f'_{syntax[syn.pos()]}_')
+                    except:
+                        pass
 
-                        # Examples
-                        for index, example in zip(range(1, 4) ,syn.examples()):
-                            st.write(index, example)       
-                        st.write('')         
+                    # Examples
+                    for index, example in zip(range(1, 4) ,syn.examples()):
+                        st.write(index, example)       
+                    st.write('')         
 
-                        # Synonyms and Antonyms
-                        synonyms = list()
-                        antonyms = list()
+                    # Synonyms and Antonyms
+                    synonyms = list()
+                    antonyms = list()
 
-                        for l in syn.lemmas():
-                            synonyms.append(l.name())
+                    for l in syn.lemmas():
+                        synonyms.append(l.name())
 
-                            if l.antonyms():
-                                for a in l.antonyms():
-                                    antonyms.append(a.name())
+                        if l.antonyms():
+                            for a in l.antonyms():
+                                antonyms.append(a.name())
 
-                        try:
-                            synonyms.remove(word)
-                        except:
-                            pass
-                            
-                        if synonyms:
-                            output_syn = ', '.join(synonyms)
-                            st.write(f'_**Synonyms**: {output_syn.replace("_", " ")}_')
-                        if antonyms:
-                            output_ant = ', '.join(antonyms)
-                            st.write(f'_**Antonyms**: {output_ant.replace("_", " ")}_')
+                    try:
+                        synonyms.remove(word)
+                    except:
+                        pass
+                        
+                    if synonyms:
+                        output_syn = ', '.join(synonyms)
+                        st.write(f'_**Synonyms**: {output_syn.replace("_", " ")}_')
+                    if antonyms:
+                        output_ant = ', '.join(antonyms)
+                        st.write(f'_**Antonyms**: {output_ant.replace("_", " ")}_')
 
 
-                        st.write('---')
+                    st.write('---')
 
-            except:
-                st.warning('Incorrect Input')
+            # except:
+            #     st.warning('Incorrect Input')
 
 
 
