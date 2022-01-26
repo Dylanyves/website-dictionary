cb6010a885a79dd1e9c75e5e813a3e48241158c4
Dylan <macyvesdylan@gmail.com>
2022-01-26 10:32:51 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,86 @@
+from pyparsing import Word
+import streamlit as st
+from nltk.corpus import wordnet
+
+st.set_page_config(
+    page_title='Dictionary',
+    page_icon='book'
+)
+
+class Content:
+    def head():
+        st.title('Dictionary')
+
+    def body():
+        with st.form(key='dict_form'):
+            word = st.text_input(label='Input Word')
+            search_btn = st.form_submit_button('Search')
+
+        if search_btn:
+            try:
+                st.subheader(f'Define - {word}')
+                syns = wordnet.synsets(word)
+
+                # Check if input is valid
+                syns[0]
+                st.write('---')
+
+                for syn in syns:
+                    if syn.examples():
+
+                        # Definition
+                        definition = syn.definition()
+                        st.write(f'**{definition.capitalize().replace(";", "")}**')
+
+                        # Syntax
+                        syntax = {
+                            'n':'Noun',
+                            'v':'Verb',
+                            'a':'Adjective',
+                            'r':'Adverb',
+                            's':'Singular'
+                        }
+                        try:
+                            st.write(f'_{syntax[syn.pos()]}_')
+                        except:
+                            pass
+
+                        # Examples
+                        for index, example in zip(range(1, 4) ,syn.examples()):
+                            st.write(index, example)       
+                        st.write('')         
+
+                        # Synonyms and Antonyms
+                        synonyms = list()
+                        antonyms = list()
+
+                        for l in syn.lemmas():
+                            synonyms.append(l.name())
+
+                            if l.antonyms():
+                                for a in l.antonyms():
+                                    antonyms.append(a.name())
+
+                        try:
+                            synonyms.remove(word)
+                        except:
+                            pass
+                            
+                        if synonyms:
+                            output_syn = ', '.join(synonyms)
+                            st.write(f'_**Synonyms**: {output_syn.replace("_", " ")}_')
+                        if antonyms:
+                            output_ant = ', '.join(antonyms)
+                            st.write(f'_**Antonyms**: {output_ant.replace("_", " ")}_')
+
+
+                        st.write('---')
+
+            except:
+                st.warning('Incorrect Input')
+
+
+
+c = Content
+c.head()
+c.body()
